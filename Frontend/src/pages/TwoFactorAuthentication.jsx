849510cac81f1588148d242b13e4b2c7456dca85
Eric Valdez <ercvldez@gmail.com>
2024-09-22 08:27:10 -0700

    Updated frontend

--- Frontend/src/pages/TwoFactorAuthentication.jsx
+++ Frontend/src/pages/TwoFactorAuthentication.jsx
@@ -7,7 +7,7 @@ import djangoApiConnection from '../django_api.js';
 function TwoFactorAuthentication() {
   //declarations
   const [profile, setProfile] = useState([]);
-  const qrcodeImage = `qrcode${profile.id}.png`;
+  const qrcodeImage = `/qrcode${profile.id}.png`;
   const navigate = useNavigate();
 
   useEffect(() => {
